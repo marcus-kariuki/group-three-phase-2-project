ec9c5fccdee548fcffb67964a48834db3e6fbd55
iamchacha021 <iamchacha021@gmail.com>
2023-01-26 00:35:08 +0300

    Add input fields for the form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import Players from "./Players";
 import Player from "./Player";
 import { Routes, Route } from "react-router-dom";
+import NewPlayer from "./NewPlayer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Players />} />
         <Route exact path="/players/:id" element={<Player />} />
+        <Route exact path = "/newplayer" element= {<NewPlayer />} />
       </Routes>
     </>
   );
