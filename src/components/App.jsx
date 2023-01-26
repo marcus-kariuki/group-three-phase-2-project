0ca6256356d760680c206b1047475b9a4aade974
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-01-26 16:09:03 +0300

    updated abot content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,26 @@ import Players from "./Players";
 import Player from "./Player";
 import { Routes, Route } from "react-router-dom";
 import NewPlayer from "./NewPlayer";
+import About from "./About";
+import React, { useState } from 'react';
 
 function App() {
+
+  const [aboutData, setAboutData] = useState({});
+
+  const handleAboutData = (data) => {
+    setAboutData(data);
+  }
   return (
     <>
-      <Navbar />
+      <Navbar onAboutData={handleAboutData} />
       <Routes>
         <Route exact path="/" element={<Players />} />
         <Route exact path="/players/:id" element={<Player />} />
         <Route exact path="/newplayer" element={<NewPlayer />} />
+        <Route exact path="/about" element={<About />} />
+        <Route path="/about" render={({ match, location, history }) => <About aboutData={aboutData} match={match} location={location} history={history} />} />
+
       </Routes>
     </>
   );
