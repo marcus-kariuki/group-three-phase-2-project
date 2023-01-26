a00473c16281191c694063109aecbe505b065f1c
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-01-26 17:04:47 +0300

    status check

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,11 @@ function About({aboutData}) {
   return (
     <div>
       <h1>About Us</h1>
-      <p>Here is some information about our organization or company.{aboutData.title}</p>
+      <p>Here is some information about our organization our company
+        Our company is based in Nairobi Kenya and with offices along ngong road inside the 
+        Ngong lane plaza.The CEO of our company is named Marcus Kariuki, there are other three co-founders
+        that are responsible to handle matters relating to this website and we give managers and coaches
+        of every team to look into the available olayers we have for transfer you can check the .{aboutData.title}</p>
       <p>{aboutData.description}</p>
     </div>
   );
